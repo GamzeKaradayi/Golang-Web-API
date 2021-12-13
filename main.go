02c637581a8f19bf2d4983e99026ee0ab1dcb50e
GamzeKaradayi <gamzekaradayi35@gmail.com>
2021-12-13 19:51:39 +0300

    using golang/mux

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/gamzekaradayi/book/models"
+	"github.com/gorilla/mux"
 )
 
 var books []models.Book
@@ -26,124 +26,102 @@ func main() {
 		{ID: 4, Name: "Fareler ve İnsanlar", Writer: "John Steinbeck", Category: "Trajedi", InStock: true},
 	}
 
-	http.HandleFunc(apiRoot+"/books", getBooks)
-	http.HandleFunc(apiRoot+"/books/", getBookByCategoryName)
-	http.HandleFunc(apiRoot+"/books/add", addBook)
-	http.HandleFunc(apiRoot+"/books/delete/", deleteBook)
-	http.HandleFunc(apiRoot+"/books/update/", updateBook)
+	myRouter := mux.NewRouter()
 
+	//GET http://localhost:8080/api/books
+	myRouter.HandleFunc(apiRoot+"/books", getBooks)
+
+	//GET http://localhost:8080/api/book/id
+	myRouter.HandleFunc(apiRoot+"/book/{id:[0-9]}", getSingleBook).Methods("GET")
+
+	//POST http://localhost:8080/api/book
+	myRouter.HandleFunc(apiRoot+"/book", createNewBook).Methods("POST")
+
+	//PUT http://localhost:8080/api/book/id
+	myRouter.HandleFunc(apiRoot+"/book/{id}", updateBook).Methods("PUT")
+
+	//DELETE http://localhost:8080/api/book/id
+	myRouter.HandleFunc(apiRoot+"/book/{id}", deleteBook).Methods("DELETE")
+
+	http.Handle("/", myRouter)
 	http.ListenAndServe(":8080", nil)
 }
 
 //bring the list of books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		output, err := json.Marshal(books)
-		checkError(err)
-		fmt.Fprintf(w, string(output))
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
+	json.NewEncoder(w).Encode(books)
 }
 
-//bring the list of books by category name
-func getBookByCategoryName(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		var result []models.Book
-		url := r.URL.Path
-		parts := strings.Split(url, "/")
+func getSingleBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key, _ := strconv.Atoi(vars["id"])
 
-		for _, p := range books {
-			if strings.EqualFold(p.Category, parts[3]) {
-				result = append(result, p)
-			}
+	//return the book encoded as JSON
+	for _, book := range books {
+		if book.ID == key {
+			json.NewEncoder(w).Encode(book)
 		}
+	}
 
-		if len(result) == 0 {
-			http.Error(w, http.StatusText(404), 404)
-		} else {
-			output, err := json.Marshal(result)
+}
+
+func createNewBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	// get the body of our POST request
+	err := json.NewDecoder(r.Body).Decode(&book)
+	checkError(err)
+
+	book.ID = rand.Intn(10000)
+	books = append(books, book)
+
+	//api response message
+	apiResult := models.Api{"Successfully added!", false}
+	output, err := json.Marshal(apiResult)
+	checkError(err)
+	fmt.Fprintf(w, string(output))
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	vars := mux.Vars(r)
+	updatedBookId, _ := strconv.Atoi(vars["id"])
+
+	for index, item := range books {
+		if item.ID == updatedBookId {
+			books = append(books[:index], books[index+1:]...)
+			err := json.NewDecoder(r.Body).Decode(&book)
+			checkError(err)
+
+			book.ID = updatedBookId
+			books = append(books, book)
+			json.NewEncoder(w).Encode(books)
+
+			//api response message
+			apiResult := models.Api{"Updated!", false}
+			output, err := json.Marshal(apiResult)
 			checkError(err)
 			fmt.Fprintf(w, string(output))
 		}
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
 
-//add a book
-func addBook(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		err := r.ParseForm()
-		checkError(err)
-
-		id, _ := strconv.Atoi(r.FormValue("id"))
-		name := r.FormValue("name")
-		writer := r.FormValue("writer")
-		category := r.FormValue("category")
-		instock, _ := strconv.ParseBool(r.FormValue("instock"))
-
-		bookItem := models.Book{ID: id, Name: name, Writer: writer, Category: category, InStock: instock}
-		books = append(books, bookItem)
-
-		apiResult := models.Api{"Successfully added!", false}
-		output, err := json.Marshal(apiResult)
-		checkError(err)
-		fmt.Fprintf(w, string(output))
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
-}
-
-//delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "DELETE" {
-		deletedBookId := r.URL.Path[len(r.URL.Path)-1:]
-		deleted, _ := strconv.Atoi(deletedBookId)
-		for index, book := range books {
-			if book.ID == deleted {
-				books = append(books[:index], books[index+1:]...)
-				apiResult := models.Api{"Deleted!", false}
-				output, err := json.Marshal(apiResult)
-				checkError(err)
-				fmt.Fprintf(w, string(output))
-			}
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	for index, book := range books {
+		if book.ID == id {
+			//book array updated after deletion
+			books = append(books[:index], books[index+1:]...)
+
+			//api response message
+			apiResult := models.Api{"Deleted!", false}
+			output, err := json.Marshal(apiResult)
+			checkError(err)
+			fmt.Fprintf(w, string(output))
 		}
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
-}
 
-//update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "PUT" {
-		var updatedBook models.Book
-		reqBody, err := ioutil.ReadAll(r.Body)
-		checkError(err)
-
-		//get id value
-		updatedBookId := r.URL.Path[len(r.URL.Path)-1:]
-		updated, _ := strconv.Atoi(updatedBookId)
-
-		json.Unmarshal(reqBody, &updatedBook)
-
-		for i, book := range books {
-			if book.ID == updated {
-				book.Name = updatedBook.Name
-				book.Writer = updatedBook.Writer
-				book.Category = updatedBook.Category
-				book.InStock = updatedBook.InStock
-				books = append(books[i:], book)
-
-				apiResult := models.Api{"Updated!", false}
-				output, err := json.Marshal(apiResult)
-				checkError(err)
-				fmt.Fprintf(w, string(output))
-			}
-		}
-	} else {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-	}
 }
 
 func checkError(err error) {
